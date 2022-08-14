00e59d23675f3255bb0be01300b64cd731fe824b
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-08-14 16:45:11 -0300

    requisito 3

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from datetime import date
+from statistics import mode
+
+
+class SimpleReport:
+    def generate(list):
+        fabrication_date = min(list, key=lambda x: x["data_de_fabricacao"])[
+            "data_de_fabricacao"
+        ]
+        expiration_date = min(
+            list,
+            key=lambda x: x["data_de_validade"]
+            if x["data_de_validade"] > date.today().strftime("%Y-%m-%d")
+            else None,
+        )["data_de_validade"]
+        company_with_more_products = mode(
+            company["nome_da_empresa"] for company in list
+        )
+        return (
+            f"Data de fabricação mais antiga: {fabrication_date}\n"
+            f"Data de validade mais próxima: {expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
