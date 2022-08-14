003633882eeeb77ab80b4aacb161e4d519b394c5
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-08-14 14:08:08 -0300

    requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        26736128,
+        "Pão Francês",
+        "Padaria Renascer",
+        "2022-13-08",
+        "2023-14-08",
+        "padaria-renascer-pao-frances_01",
+        "Tempera ambiente",
+    )
+
+    assert produto.id == 26736128
+    assert produto.nome_do_produto == "Pão Francês"
+    assert produto.nome_da_empresa == "Padaria Renascer"
+    assert produto.data_de_fabricacao == "2022-13-08"
+    assert produto.data_de_validade == "2023-14-08"
+    assert produto.numero_de_serie == "padaria-renascer-pao-frances_01"
+    assert produto.instrucoes_de_armazenamento == "Tempera ambiente"
